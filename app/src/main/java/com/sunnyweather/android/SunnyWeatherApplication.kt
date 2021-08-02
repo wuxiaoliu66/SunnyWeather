efc31bf9e82c8e1f2e44bf9595b62c503616fab7
wuruoke <wuruoke@kuaishou.com>
2021-08-02 15:54:12 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,17 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+  companion object {
+    @SuppressLint("StaticFieldLeak")
+    lateinit var context: Context
+    const val TOKEN = "7KlicJFJqOmm3iIn"
+  }
+  override fun onCreate() {
+    super.onCreate()
+    context = applicationContext
+  }
+}
