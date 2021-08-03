b98f5c10fb8ed43046c4d7fc475ae2521ae6138b
wuruoke <wuruoke@kuaishou.com>
2021-08-03 14:33:27 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,13 +1,17 @@
 package com.sunnyweather.android.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sunnyweather.android.R
@@ -40,6 +44,7 @@ class WeatherActivity : AppCompatActivity() {
     if (viewModel.placeName.isEmpty()) {
       viewModel.placeName = intent.getStringExtra("place_name") ?: ""
     }
+
     viewModel.weatherLiveData.observe(this, Observer { result ->
       val weather = result.getOrNull()
       if (weather != null) {
@@ -48,9 +53,33 @@ class WeatherActivity : AppCompatActivity() {
         Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
         result.exceptionOrNull()?.printStackTrace()
       }
+      swipeRefresh.isRefreshing = false
     })
-    viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    swipeRefresh.setColorSchemeResources(R.color.teal_700)
+    refreshWeather()
+    swipeRefresh.setOnRefreshListener {
+      refreshWeather()
+    }
+
+    navBtn.setOnClickListener {
+      drawerLayout.openDrawer(GravityCompat.START)
+    }
+    drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+      override fun onDrawerStateChanged(newState: Int) {}
+      override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+      override fun onDrawerOpened(drawerView: View) {}
+      override fun onDrawerClosed(drawerView: View) {
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+      }
+    })
   }
+
+  fun refreshWeather() {
+    viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    swipeRefresh.isRefreshing = true
+  }
+
   private fun showWeatherInfo(weather: Weather) {
     placeName.text = viewModel.placeName
     val realtime = weather.realtime
